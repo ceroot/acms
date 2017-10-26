ce1b09094be89a883eca8a385ec3ce2db0544fb8
ceroot <ceroot@163.com>
2017-10-26 15:35:48 +0800

    init
    init，初始文件

---
+++ config/console/checklogin.php
@@ -0,0 +1,27 @@
+<?php
+// +----------------------------------------------------------------------+
+// | CYCMS                                                                |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2017 http://www.benweng.com All rights reserved.       |
+// +----------------------------------------------------------------------+
+// | Authors: SpringYang [ceroot@163.com]                                 |
+// +----------------------------------------------------------------------+
+/**
+ *
+ * @filename  checklogin.php
+ * @authors   SpringYang
+ * @email     ceroot@163.com
+ * @QQ        525566309
+ * @date      2017-10-26 11:33:36
+ * @site      http://www.benweng.com
+ * @version   $Id$
+ */
+
+return [
+    'not_login_controller' => [
+
+    ],
+    'not_login_action'     => [
+        'fds',
+    ],
+];
---
+++ config/weblog.php
@@ -0,0 +1,27 @@
+<?php
+// +----------------------------------------------------------------------+
+// | CYCMS                                                                |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2017 http://www.benweng.com All rights reserved.       |
+// +----------------------------------------------------------------------+
+// | Authors: SpringYang [ceroot@163.com]                                 |
+// +----------------------------------------------------------------------+
+/**
+ *
+ * @filename  weblog.php
+ * @authors   SpringYang
+ * @email     ceroot@163.com
+ * @QQ        525566309
+ * @date      2017-10-26 11:57:43
+ * @site      http://www.benweng.com
+ * @version   $Id$
+ */
+
+return [
+    'status'             => true,
+    'not_log_method'     => [],
+    'not_log_module'     => [],
+    'not_log_controller' => [],
+    'not_log_action'     => [],
+    'not_log_action_exc' => [],
+];
