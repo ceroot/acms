23f4030ea8f0d4ac9ce9e882a947375442f7ae42
ceroot <ceroot@163.com>
2017-10-27 15:33:21 +0800

    add file
    add file

---
+++ application/console/model/Config.php
@@ -0,0 +1,26 @@
+<?php
+// +----------------------------------------------------------------------+
+// | CYCMS                                                                |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2017 http://www.benweng.com All rights reserved.       |
+// +----------------------------------------------------------------------+
+// | Authors: SpringYang [ceroot@163.com]                                 |
+// +----------------------------------------------------------------------+
+/**
+ *
+ * @filename  Config.php
+ * @authors   SpringYang
+ * @email     ceroot@163.com
+ * @QQ        525566309
+ * @date      2017-10-27 12:19:01
+ * @site      http://www.benweng.com
+ * @version   $Id$
+ */
+namespace app\console\model;
+
+use app\common\model\Extend;
+
+class Config extends Extend
+{
+
+}
