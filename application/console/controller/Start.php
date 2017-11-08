988fbddae91ed0ee6049c80f0cd519f28a07065c
ceroot <ceroot@163.com>
2017-11-09 01:53:55 +0800

    update
    update

--- application/console/controller/Start.php
+++ application/console/controller/Start.php
@@ -17,6 +17,7 @@
 namespace app\console\controller;
 
 use app\facade\User;
+use QL\QueryList;
 use think\Controller;
 use think\facade\App;
 use think\facade\Hook;
@@ -85,7 +86,9 @@ class Start extends Controller
             $authcode = authcode($time);
             $this->success('登录成功', url('console/index/index?time=' . $time . '&authcode=' . $authcode), ['error_num' => 0]);
         } else {
+            $text = '<p>　　根据《省教育厅省财政厅 省人力资源和社会保障厅 省机构编制委员会办公室关于印发〈贵州省2014年农村义务教育阶段学校教师特设岗位计划实施方案〉的通知》（黔教师发〔2014〕113号）、《州人社局 州教育局关于办理“特岗教师”聘用手续有关问题的通知》（黔南人社通〔2012〕75号）文件精神和县人民政府《惠水县2014年特岗计划承诺书》的相关承诺，经审核，目前有赵成妹等251名“特岗教师”符合服务期满接转到当地任教的条件，经惠水县人民政府常务会议研究，同意将赵成妹等251名“特岗教师”接转为我县正式教师。现进行公示，公示期自发布之日起七个工作日。</p><p>　　如对接转人员有异议，请在公示期内向惠水县人力资源和社会保障局人事科反映。</p><p>　　联系电话： 0854&#8212;6221862（惠水县人力资源和社会保障局人事科）</p><p>　　监督电话： 0854&#8212;6221197 （惠水县纪委监察局）</p><p>　　附件：惠水县2014年特岗教师服务期满接转为正式教师花名册.xlsx</p><p>&#160;</p><p style="text-align:right;">　　惠水县人力资源和社会保障局</p><p style="text-align:right;">　　2017年11月8日</p>';
             $this->assign('verifyhtml', $this->verifyhtml());
+            $this->assign('text', $text);
             return $this->fetch();
         }
     }
@@ -178,5 +181,127 @@ class Start extends Controller
         $loginurl = url('console/start/index?time=' . time()) . '?backurl=' . $backurl;
 
         return $this->success('注销成功', $loginurl);
+    }
+
+    public function ttt()
+    {
+        $data = QueryList::get('http://www.163gz.com/js/163.html')->rules([
+            'link' => ['.STYLE4>a', 'href', '', function ($content) {
+                //利用回调函数补全相对链接
+                $baseUrl = 'http://cms.querylist.cc';
+                return $content;
+            }],
+            'text' => ['.STYLE4>a', 'text'],
+        ])->query()->getData();
+
+        // $data = QueryList::rules([
+        //     'txt' => ['a', 'text'],
+        // ])->get('https://top.etao.com')->encoding('UTF-8', 'GB2312')->query()->getData();
+        //打印结果
+        $temp = '';
+        foreach ($data->all() as $value) {
+            # code...
+            $temp .= $value['text'];
+
+        }
+        // dump($temp);
+
+        // die;
+        $path    = "C:\\Users\\ceroo\\Desktop\\test\\one.txt";
+        $content = "one for all";
+        file_put_contents($path, $temp);
+        if (file_exists($path)) {echo "ok";} else {echo "ng";}
+
+    }
+
+    public function te()
+    {
+        // $data = QueryList::get('http://cms.querylist.cc/bizhi/460.html')->find('img')->attrs('src');
+        //打印结果
+        // dump($data->all());
+
+        // $html = file_get_contents('http://www.163gz.com/js/163.html');
+        // //然后可以把页面源码或者HTML片段传给QueryList
+        // $data = QueryList::html($html)->rules([ //设置采集规则
+        //     // 采集所有a标签的href属性
+        //     'link' => ['a', 'href'],
+        //     // 采集所有a标签的文本内容
+        //     'text' => ['a', 'text'],
+        // ])->query()->getData();
+
+        // $data = QueryList::get('http://www.163gz.com/js/163.html')->rules([
+        //     'link' => ['.STYLE4>a', 'href', '', function ($content) {
+        //         //利用回调函数补全相对链接
+        //         $baseUrl = 'http://cms.querylist.cc';
+        //         return $content;
+        //     }],
+        //     'text' => ['.STYLE4>a', 'text'],
+        // ])->query()->encoding('GB2312')->getData();
+
+        // $data = QueryList::rules([
+        //     'text' => ['#zoom', 'text'],
+        // ])->get('http://www.163gz.com/gzzp8/zkxx/20171108/195941.shtml')->encoding('UTF-8', 'GB2312')->query()->getData();
+        //打印结果
+        $data = QueryList::get('http://www.163gz.com/gzzp8/zkxx/20171108/195941.shtml')->rules([
+            'title' => ['#zoom h3', 'text'],
+            'text'  => ['#zoom', 'text', 'p', function ($content) {
+                $content = strtolower($content);
+                $pattern = "/<p[^>]*>([^\<|\>]*)<\/p>/is";
+                preg_match_all($pattern, $content, $Html);
+                // dump($Html[0]);
+                $temp = '';
+                foreach ($Html[0] as $value) {
+                    preg_match($pattern, $value, $p1);
+                    if (strpos($value, 'text-align: right')) {
+                        $temp .= '<p style="text-align:right;">' . $p1[1] . '</p>';
+                    } else {
+                        $temp .= '<p>' . $p1[1] . '</p>';
+                    }
+                }
+                return $temp;
+            }],
+        ])->query()->getData();
+
+        $ddd = $data->all();
+        $ddd = $ddd[0];
+        // dump($ddd['text']);
+        // die;
+        $path = "C:\\Users\\ceroo\\Desktop\\test\\" . time() . ".txt";
+        file_put_contents($path, $ddd['text']);
+        if (file_exists($path)) {
+            echo "ok";
+        } else {
+            echo "ng";
+        }
+
+        // dump($data->all());
+    }
+
+    public function t2()
+    {
+        $content = '惠水县关于接转2014年“特岗教师”为正式教师的公示
+<p style="WHITE-SPACE: normal; WORD-SPACING: 0px; TEXT-TRANSFORM: none; COLOR: rgb(0,0,0); PADDING-BOTTOM: 0px; PADDING-TOP: 0px; FONT: 16px/28px 宋体, sans-serif; PADDING-LEFT: 0px; WIDOWS: 1; MARGIN: 10px 0px; LETTER-SPACING: normal; PADDING-RIGHT: 0px; TEXT-INDENT: 0px; -webkit-text-stroke-width: 0px">　　根据《省教育厅省财政厅 省人力资源和社会保障厅 省机构编制委员会办公室关于印发〈贵州省2014年农村义务教育阶段学校教师特设岗位计划实施方案〉的通知》（黔教师发〔2014〕113号）、《州人社局 州教育局关于办理“特岗教师”聘用手续有关问题的通知》（黔南人社通〔2012〕75号）文件精神和县人民政府《惠水县2014年特岗计划承诺书》的相关承诺，经审核，目前有赵成妹等251名“特岗教师”符合服务期满接转到当地任教的条件，经惠水县人民政府常务会议研究，同意将赵成妹等251名“特岗教师”接转为我县正式教师。现进行公示，公示期自发布之日起七个工作日。</p>
+<p style="WHITE-SPACE: normal; WORD-SPACING: 0px; TEXT-TRANSFORM: none; COLOR: rgb(0,0,0); PADDING-BOTTOM: 0px; PADDING-TOP: 0px; FONT: 16px/28px 宋体, sans-serif; PADDING-LEFT: 0px; WIDOWS: 1; MARGIN: 10px 0px; LETTER-SPACING: normal; PADDING-RIGHT: 0px; TEXT-INDENT: 0px; -webkit-text-stroke-width: 0px">　　如对接转人员有异议，请在公示期内向惠水县人力资源和社会保障局人事科反映。</p>
+<p style="WHITE-SPACE: normal; WORD-SPACING: 0px; TEXT-TRANSFORM: none; COLOR: rgb(0,0,0); PADDING-BOTTOM: 0px; PADDING-TOP: 0px; FONT: 16px/28px 宋体, sans-serif; PADDING-LEFT: 0px; WIDOWS: 1; MARGIN: 10px 0px; LETTER-SPACING: normal; PADDING-RIGHT: 0px; TEXT-INDENT: 0px; -webkit-text-stroke-width: 0px">　　联系电话： 0854&#8212;6221862（惠水县人力资源和社会保障局人事科）</p>
+<p style="WHITE-SPACE: normal; WORD-SPACING: 0px; TEXT-TRANSFORM: none; COLOR: rgb(0,0,0); PADDING-BOTTOM: 0px; PADDING-TOP: 0px; FONT: 16px/28px 宋体, sans-serif; PADDING-LEFT: 0px; WIDOWS: 1; MARGIN: 10px 0px; LETTER-SPACING: normal; PADDING-RIGHT: 0px; TEXT-INDENT: 0px; -webkit-text-stroke-width: 0px">　　监督电话： 0854&#8212;6221197 （惠水县纪委监察局）</p>
+<p style="WHITE-SPACE: normal; WORD-SPACING: 0px; TEXT-TRANSFORM: none; COLOR: rgb(0,0,0); PADDING-BOTTOM: 0px; PADDING-TOP: 0px; FONT: 16px/28px 宋体, sans-serif; PADDING-LEFT: 0px; WIDOWS: 1; MARGIN: 10px 0px; LETTER-SPACING: normal; PADDING-RIGHT: 0px; TEXT-INDENT: 0px; -webkit-text-stroke-width: 0px">　　附件：惠水县2014年特岗教师服务期满接转为正式教师花名册.xlsx</p>
+<p style="WHITE-SPACE: normal; WORD-SPACING: 0px; TEXT-TRANSFORM: none; COLOR: rgb(0,0,0); PADDING-BOTTOM: 0px; PADDING-TOP: 0px; FONT: 16px/28px 宋体, sans-serif; PADDING-LEFT: 0px; WIDOWS: 1; MARGIN: 10px 0px; LETTER-SPACING: normal; PADDING-RIGHT: 0px; TEXT-INDENT: 0px; -webkit-text-stroke-width: 0px">&#160;</p>
+<p style="WHITE-SPACE: normal; WORD-SPACING: 0px; TEXT-TRANSFORM: none; COLOR: rgb(0,0,0); PADDING-BOTTOM: 0px; TEXT-ALIGN: right; PADDING-TOP: 0px; FONT: 16px/28px 宋体, sans-serif; PADDING-LEFT: 0px; WIDOWS: 1; MARGIN: 10px 0px; LETTER-SPACING: normal; PADDING-RIGHT: 0px; TEXT-INDENT: 0px; -webkit-text-stroke-width: 0px">　　惠水县人力资源和社会保障局</p>
+<p style="WHITE-SPACE: normal; WORD-SPACING: 0px; TEXT-TRANSFORM: none; COLOR: rgb(0,0,0); PADDING-BOTTOM: 0px; TEXT-ALIGN: right; PADDING-TOP: 0px; FONT: 16px/28px 宋体, sans-serif; PADDING-LEFT: 0px; WIDOWS: 1; MARGIN: 10px 0px; LETTER-SPACING: normal; PADDING-RIGHT: 0px; TEXT-INDENT: 0px; -webkit-text-stroke-width: 0px">　　2017年11月8日</p>
+';
+        $content = strtolower($content);
+        $pattern = "/<p[^>]*>([^\<|\>]*)<\/p>/is";
+        preg_match_all($pattern, $content, $Html);
+        // dump($Html[0]);
+        $dd = '';
+        foreach ($Html[0] as $value) {
+            preg_match($pattern, $value, $p1);
+            if (strpos($value, 'text-align: right')) {
+                $dd .= '<p style="text-align:right;">' . $p1[1] . '</p>';
+            } else {
+                $dd .= '<p>' . $p1[1] . '</p>';
+            }
+        }
+        dump($dd);
     }
 }
