b812bcfd0bcc90b48525fb2ab0ebfe03b2a36927
ceroot <ceroot@163.com>
2017-12-29 16:21:20 +0800

    更新不记录网站日志操作页面
    更新不记录网站日志操作页面

--- application/common/behavior/WebLog.php
+++ application/common/behavior/WebLog.php
@@ -26,152 +26,161 @@ use think\facade\Log;
 use think\facade\Request;
 use think\facade\Session;
 
-class WebLog {
-	/**
-	 * { function_name function_description}
-	 * @Author   SpringYang
-	 * @DateTime 2017-10-23T10:48:09+0800
-	 * @param    [type]                   $param [description]
-	 * @param    [type]                   $dd    [description]
-	 * @return   [type]                          [description]
-	 */
-	public function run($param) {
-		// Log::record("[ 系统配置 ]：初始化成功");
-		// $dd = getrandom();
-		// dump($dd);die;
-		$this->webLog();
+class WebLog
+{
+    /**
+     * { function_name function_description}
+     * @Author   SpringYang
+     * @DateTime 2017-10-23T10:48:09+0800
+     * @param    [type]                   $param [description]
+     * @param    [type]                   $dd    [description]
+     * @return   [type]                          [description]
+     */
+    public function run($param)
+    {
+        // Log::record("[ 系统配置 ]：初始化成功");
+        // $dd = getrandom();
+        // dump($dd);die;
+        $this->webLog();
 
-	}
+    }
 
-	/**
-	 * { function_name function_description}
-	 * @Author   SpringYang
-	 * @DateTime 2017-10-23T10:48:12+0800
-	 * @return   [type]                   [description]
-	 */
-	private function webLog() {
+    /**
+     * { function_name function_description}
+     * @Author   SpringYang
+     * @DateTime 2017-10-23T10:48:12+0800
+     * @return   [type]                   [description]
+     */
+    private function webLog()
+    {
 
-		// 网站日志记录开关判断
-		if (!Config::get('weblog.status')) {
-			return true;
-		}
+        // 网站日志记录开关判断
+        if (!Config::get('weblog.status')) {
+            return true;
+        }
 
-		// 搜索引擎蜘蛛判断
-		if ($this->checkrobot()) {
-			return true;
-		}
+        // 搜索引擎蜘蛛判断
+        if ($this->checkrobot()) {
+            return true;
+        }
+        // dump(Request::controller());die;
+        if (Request::controller() == 'WebLog') {
+            return true;
+        }
 
-		// 不需要记录的请求方式 string
-		$not_log_method_default = [];
-		$not_log_method_config = Config::get('weblog.not_log_method') ?: [];
-		$not_log_method = array_merge($not_log_method_default, $not_log_method_config);
+        // 不需要记录的请求方式 string
+        $not_log_method_default = [];
+        $not_log_method_config  = Config::get('weblog.not_log_method') ?: [];
+        $not_log_method         = array_merge($not_log_method_default, $not_log_method_config);
 
-		// 不需要记录的模块 'module'
-		$not_log_module_default = [];
-		$not_log_module_config = Config::get('weblog.not_log_module') ?: [];
-		$not_log_module = array_merge($not_log_module_default, $not_log_module_config);
+        // 不需要记录的模块 'module'
+        $not_log_module_default = [];
+        $not_log_module_config  = Config::get('weblog.not_log_module') ?: [];
+        $not_log_module         = array_merge($not_log_module_default, $not_log_module_config);
 
-		// 不需要记录的控制器 'module/controller'
-		$not_log_controller_default = [];
-		$not_log_controller_config = Config::get('weblog.not_log_controller') ?: [];
-		$not_log_controller = array_merge($not_log_controller_default, $not_log_controller_config);
+        // 不需要记录的控制器 'module/controller'
+        $not_log_controller_default = [];
+        $not_log_controller_config  = Config::get('weblog.not_log_controller') ?: [];
+        $not_log_controller         = array_merge($not_log_controller_default, $not_log_controller_config);
 
-		// 不需要记录的方法 'module/controller/action'
-		$not_log_action_default = [];
-		$not_log_action_config = Config::get('weblog.not_log_action') ?: [];
-		$not_log_action = array_merge($not_log_action_default, $not_log_action_config);
+        // 不需要记录的方法 'module/controller/action'
+        $not_log_action_default = [];
+        $not_log_action_config  = Config::get('weblog.not_log_action') ?: [];
+        $not_log_action         = array_merge($not_log_action_default, $not_log_action_config);
 
-		// 不需要记录的方法（特殊情况） 'action'
-		$not_log_action_exc_default = [
-			'test', // 测试方法
-			'showverify', // 验证码
-			'setcollapsed', // 侧边栏
-			'getcollapsed',
-		];
-		$not_log_action_exc_config = Config::get('weblog.not_log_action_exc') ?: []; // 取得配置
-		$not_log_action_exc = array_merge($not_log_action_exc_default, $not_log_action_exc_config);
+        // 不需要记录的方法（特殊情况） 'action'
+        $not_log_action_exc_default = [
+            'test', // 测试方法
+            'showverify', // 验证码
+            'setcollapsed', // 侧边栏
+            'getcollapsed',
+        ];
+        $not_log_action_exc_config = Config::get('weblog.not_log_action_exc') ?: []; // 取得配置
+        $not_log_action_exc        = array_merge($not_log_action_exc_default, $not_log_action_exc_config);
 
-		if (
-			(Config::get('app_debug') && Request::param('notlog') == 1) || // 当开发模式开启并且 param 参数 notlog 为 1 时不记录
-			in_array(strtolower(Request::action()), $not_log_action_exc) || // 一些特殊方法的处理
-			in_array(strtolower(Request::method()), $not_log_method) ||
-			in_array(strtolower(Request::module()), $not_log_module) ||
-			in_array(strtolower(Request::module() . '/' . Request::controller()), $not_log_controller) ||
-			in_array(strtolower(Request::module() . '/' . Request::controller() . '/' . Request::action()), $not_log_action)
-		) {
-			Log::record("[ 网站日志记录 ]：此访问不记录网站日志");
-			return true;
-		}
+        if (
+            (Config::get('app_debug') && Request::param('notlog') == 1) || // 当开发模式开启并且 param 参数 notlog 为 1 时不记录
+            in_array(strtolower(Request::action()), $not_log_action_exc) || // 一些特殊方法的处理
+            in_array(strtolower(Request::method()), $not_log_method) ||
+            in_array(strtolower(Request::module()), $not_log_module) ||
+            in_array(strtolower(Request::module() . '/' . Request::controller()), $not_log_controller) ||
+            in_array(strtolower(Request::module() . '/' . Request::controller() . '/' . Request::action()), $not_log_action)
+        ) {
+            Log::record("[ 网站日志记录 ]：此访问不记录网站日志");
+            return true;
+        }
 
-		// 不需要记录数据的方法 'module/controller/action'
-		$not_log_data = Config::get('weblog.not_log_data') ?: [];
+        // 不需要记录数据的方法 'module/controller/action'
+        $not_log_data = Config::get('weblog.not_log_data') ?: [];
 
-		// 数据处理
-		if (in_array(strtolower(Request::module() . '/' . Request::controller() . '/' . Request::action()), $not_log_data)) {
-			$tempData = '';
-		} else {
-			$paramData = Request::param(); // 取得提交数据
-			$tempData = [];
-			foreach ($paramData as $k => $v) {
-				if (is_string($v) && isset($v{50})) {
-					$v = mb_substr($v, 0, 50);
-				}
-				$tempData[$k] = $v;
-			}
-			$paramData = $tempData;
-		}
+        // 数据处理
+        if (in_array(strtolower(Request::module() . '/' . Request::controller() . '/' . Request::action()), $not_log_data)) {
+            $tempData = '';
+        } else {
+            $paramData = Request::param(); // 取得提交数据
+            $tempData  = [];
+            foreach ($paramData as $k => $v) {
+                if (is_string($v) && isset($v{50})) {
+                    $v = mb_substr($v, 0, 50);
+                }
+                $tempData[$k] = $v;
+            }
+            $paramData = $tempData;
+        }
 
-		// 写入数据组合
-		$data = [
-			'uid' => Session::get('user_auth.id') ?: 0,
-			'os' => get_os(),
-			'browser' => get_broswer(),
-			'url' => Request::url(),
-			'module' => Request::module(),
-			'controller' => Request::controller(),
-			'action' => Request::action(),
-			'method' => Request::method(),
-			'data' => serialize($paramData),
-			'device' => get_visit_source(),
-		];
+        // 写入数据组合
+        $data = [
+            'uid'        => Session::get('user_auth.id') ?: 0,
+            'os'         => get_os(),
+            'browser'    => get_broswer(),
+            'url'        => Request::url(),
+            'module'     => Request::module(),
+            'controller' => Request::controller(),
+            'action'     => Request::action(),
+            'method'     => Request::method(),
+            'data'       => serialize($paramData),
+            'device'     => get_visit_source(),
+        ];
 
-		// App::model('WebLog')->create($data);
-		$model = new WebLogModel;
-		if ($model->create($data)) {
-			Log::record("[ 网站日志记录 ]：网站日志数据记录成功");
-		} else {
-			Log::record("[ 网站日志记录 ]：网站日志数据记录失败");
-		};
+        // App::model('WebLog')->create($data);
+        $model = new WebLogModel;
+        if ($model->create($data)) {
+            Log::record("[ 网站日志记录 ]：网站日志数据记录成功");
+        } else {
+            Log::record("[ 网站日志记录 ]：网站日志数据记录失败");
+        };
 
-	}
+    }
 
-	private function checkrobot($useragent = '') {
-		static $kw_spiders = array('bot', 'crawl', 'spider', 'slurp', 'sohu-search', 'lycos', 'robozilla');
-		static $kw_browsers = array('msie', 'netscape', 'opera', 'konqueror', 'mozilla');
+    private function checkrobot($useragent = '')
+    {
+        static $kw_spiders  = array('bot', 'crawl', 'spider', 'slurp', 'sohu-search', 'lycos', 'robozilla');
+        static $kw_browsers = array('msie', 'netscape', 'opera', 'konqueror', 'mozilla');
 
-		$useragent = strtolower(empty($useragent) ? $_SERVER['HTTP_USER_AGENT'] : $useragent);
-		if (strpos($useragent, 'http://') === false && $this->dstrpos($useragent, $kw_browsers)) {
-			return false;
-		}
+        $useragent = strtolower(empty($useragent) ? $_SERVER['HTTP_USER_AGENT'] : $useragent);
+        if (strpos($useragent, 'http://') === false && $this->dstrpos($useragent, $kw_browsers)) {
+            return false;
+        }
 
-		if ($this->dstrpos($useragent, $kw_spiders)) {
-			return true;
-		}
+        if ($this->dstrpos($useragent, $kw_spiders)) {
+            return true;
+        }
 
-		return false;
-	}
-	private function dstrpos($string, $arr, $returnvalue = false) {
-		if (empty($string)) {
-			return false;
-		}
+        return false;
+    }
+    private function dstrpos($string, $arr, $returnvalue = false)
+    {
+        if (empty($string)) {
+            return false;
+        }
 
-		foreach ((array) $arr as $v) {
-			if (strpos($string, $v) !== false) {
-				$return = $returnvalue ? $v : true;
-				return $return;
-			}
-		}
-		return false;
-	}
+        foreach ((array) $arr as $v) {
+            if (strpos($string, $v) !== false) {
+                $return = $returnvalue ? $v : true;
+                return $return;
+            }
+        }
+        return false;
+    }
 
 }
